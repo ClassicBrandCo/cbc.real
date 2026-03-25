3c76aa09bb00b8c354121f5f653b7f77dd3bac7e
ClassicBrandCo <classicbrandco@gmail.com>
2026-03-25 15:44:03 +0300

    Updates

--- assets/arrow-right-B_gc6AhK.js
+++ assets/arrow-right-B_gc6AhK.js
@@ -1,4 +1,4 @@
-import{c as r}from"./index-26t5ITd5.js";/**
+import{c as r}from"./index-BTO5j_qM.js";/**
  * @license lucide-react v0.462.0 - ISC
  *
  * This source code is licensed under the ISC license.
--- assets/check-OTuOtg6W.js
+++ assets/check-OTuOtg6W.js
@@ -1,4 +1,4 @@
-import{c}from"./index-26t5ITd5.js";/**
+import{c}from"./index-BTO5j_qM.js";/**
  * @license lucide-react v0.462.0 - ISC
  *
  * This source code is licensed under the ISC license.
